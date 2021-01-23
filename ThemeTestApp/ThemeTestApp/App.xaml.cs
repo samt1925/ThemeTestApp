cde9090f0473e79df40969662b0ac6f06ae75161
Samet <samet.sigirci@tulpar.io>
2021-01-23 22:59:18 +0300

    Add project files.

---
+++ ThemeTestApp/ThemeTestApp/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ThemeTestApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+            Current.RequestedThemeChanged += Current_RequestedThemeChanged;
+        }
+
+        private void Current_RequestedThemeChanged(object sender, AppThemeChangedEventArgs e)
+        {
+            App.Current.MainPage.DisplayAlert("App Alert", App.Current.UserAppTheme.ToString(), "ok");
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ ThemeTestApp/ThemeTestApp/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ThemeTestApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            App.Current.RequestedThemeChanged += Current_RequestedThemeChanged;
+        }
+        private void Current_RequestedThemeChanged(object sender, AppThemeChangedEventArgs e)
+        {
+            App.Current.MainPage.DisplayAlert("MainPage Alert", App.Current.UserAppTheme.ToString(), "ok");
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            App.Current.UserAppTheme = OSAppTheme.Light;
+        }
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            App.Current.UserAppTheme = OSAppTheme.Dark;
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+            App.Current.UserAppTheme = OSAppTheme.Unspecified;
+        }
+    }
+}
